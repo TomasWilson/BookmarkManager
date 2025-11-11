798e30f9c25fda19bfcc23a697619dd08b63a385
TomasWilson <vonstuckrad@gmail.com>
2025-11-11 14:46:41 +0100

    beginning to rework UI, let the user save his api key in chrome.storage.sync

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,4 +1,31 @@
 
+
+const button_save_api = document.querySelector("#save_api_key");
+const save_icon = button_save_api.querySelector("i");
+const api_key_input = document.querySelector("#api_key_input");
+
+
+var api_key = "";
+const { GEMINI_API_KEY } = await chrome.storage.sync.get("GEMINI_API_KEY");
+if(GEMINI_API_KEY) api_key = GEMINI_API_KEY;
+api_key_input.value = api_key;
+console.log("api_key = ", api_key);
+
+api_key_input.addEventListener("change", () => {
+    api_key = api_key_input.value;
+});
+
+button_save_api.addEventListener("click", async () => {
+    button_save_api.classList.add("saved");
+    save_icon.classList.replace("fa-save", "fa-check");
+    await chrome.storage.sync.set({ "GEMINI_API_KEY": api_key });
+
+    setTimeout(() => {
+        button_save_api.classList.remove("saved");
+        save_icon.classList.replace("fa-check", "fa-save");
+    }, 1500);
+});
+
 document.getElementById("update").addEventListener("click", () => {
 
     var source_folder_name = document.getElementById("source_folder").value;
@@ -27,3 +54,4 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     }
 
 });
+
